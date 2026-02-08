8c7bff6d852c726dc2dc1b85b619814a48f193c5
Barath A P <barath32145@gmail.com>
2026-02-08 22:43:35 +0530

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -2,9 +2,9 @@ import streamlit as st
 import pandas as pd
 from sqlalchemy import create_engine, text
 
-engine = create_engine(
-    "mysql+pymysql://root:12345@localhost:3306/EARTHQUAKE"
-)
+db_url = f"mysql+pymysql://{db['user']}:{db['password']}@{db['host']}:{db['port']}/{db['database']}"
+
+engine = create_engine(db_url)
 
 
 st.title("Earrthquake data analysis")
@@ -314,6 +314,7 @@ if st.button("click to view",key = "key28"):
         df = pd.read_sql(query, conn)
         st.dataframe(df)
     
+
 
 
 
